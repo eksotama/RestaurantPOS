3f13aebc3e7d67586fb9d83423df895e14198660
Subal Sapkota <subal.sapkota@selu.com>
2019-03-31 22:07:47 -0500

    Added Cook Feature (Model, DTO, Mapper)

---
+++ SP19.P05.Web/Features/Cook/Cook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SP19.P05.Common;
+using SP19.P05.Web.Features.Authorization;
+
+namespace SP19.P05.Web.Features.Cook
+{
+    public class Cook
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+    }
+}
---
+++ SP19.P05.Web/Features/Cook/CookMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+
+namespace SP19.P05.Web.Features.Cook
+{
+    public class CookMapping : Profile
+    {
+        public CookMapping()
+        {
+            CreateMap<Cook, CookDto>()
+                .ReverseMap()
+                .ForMember(x => x.Id, o => o.Ignore()); //don't allow Id to be assigned by DTOs
+
+            CreateMap<CreateCookDto, CookDto>();
+            CreateMap<CreateCookDto, Cook>();
+        }
+    }
+}
